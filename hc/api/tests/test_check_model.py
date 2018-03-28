416e1a5632da9f66b50355fac706a9cd2745d070
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-03-28 15:59:45 +0300

    [#131049127] Fixed: Test that when a new check is created, it is not in the grace period

--- hc/api/tests/test_check_model.py
+++ hc/api/tests/test_check_model.py
@@ -5,10 +5,18 @@ from django.utils import timezone
 from hc.api.models import Check, Notification, DEFAULT_TIMEOUT, DEFAULT_GRACE
 
 
-class CheckModelTestCase(TestCase):
-
+class CheckModelTestCase(TestCase):    
+         
     def test_it_strips_tags(self):
         check = Check()
+
+        check = Check()
+        check.last_ping = timezone.now() 
+        check.timeout = timezone.timedelta(days=1) # 
+        check.grace = timezone.timedelta(hours=1)          
+        
+        up_ends = check.last_ping + check.timeout
+        grace_ends = up_ends + check.grace  
 
         check.tags = " foo  bar "
         self.assertEquals(check.tags_list(), ["foo", "bar"])
@@ -18,16 +26,13 @@ class CheckModelTestCase(TestCase):
 
     def test_status_works_with_grace_period(self):
         check = Check()
-        my_status = "up"
-        check.status = my_status
-        # r = self.client.get("/check/")        
         check.last_ping = timezone.now() 
-        check.timeout = timezone.timedelta(days=1)
-        check.grace = timezone.timedelta(hours=1)           
+        check.timeout = timezone.timedelta(days=1) # 
+        check.grace = timezone.timedelta(hours=1)          
         
         up_ends = check.last_ping + check.timeout
-        grace_ends = up_ends + check.grace             
-
+        grace_ends = up_ends + check.grace  
+        
         self.assertEqual(check.in_grace_period(), up_ends < check.last_ping < grace_ends)
         self.assertEqual(check.get_status(), check.status)
 
@@ -37,13 +42,38 @@ class CheckModelTestCase(TestCase):
         ### The above 2 asserts fail. Make them pass
 
     def test_paused_check_is_not_in_grace_period(self):
-        check = Check()
+        check = Check()      
 
-        check.status = "up"
-        check.last_ping = timezone.now() - timedelta(days=1, minutes=30)
-        self.assertTrue(check.in_grace_period())
+        my_status = ["up", "paused"]
+        for stat in my_status:
+            if stat == "up":
+                check.status = stat
+                check.last_ping = timezone.now()
+                check.timeout = timezone.timedelta(days=1)
+                check.grace_period = timezone.timedelta(hours=1)
 
-        check.status = "paused"
-        self.assertFalse(check.in_grace_period())
+                up_ends = check.last_ping + check.timeout
+                grace_ends = up_ends + check.grace
+
+                self.assertEqual(check.get_status(), "up")
+                self.assertEqual(check.in_grace_period(), up_ends < check.last_ping < grace_ends)
+            else:
+                check.status = stat
+                check.last_ping = timezone.now()
+                check.timeout = timezone.timedelta(days=1)
+                check.grace_period = timezone.timedelta(hours=1)
+
+                up_ends = check.last_ping + check.timeout
+                grace_ends = up_ends + check.grace
+
+                self.assertEqual(check.get_status(), "paused")
+                self.assertEqual(check.in_grace_period(), up_ends < check.last_ping < grace_ends)
+
 
     ### Test that when a new check is created, it is not in the grace period
+    def test_new_check_not_in_grace_period(self):
+        check = Check()
+        check.last_ping = timezone.now()
+        check.grace_period = timezone.timedelta(hours=1)     
+
+        self.assertFalse(check.in_grace_period(), False) # less than the grace period and not enough time for up status
