2fc9f5f06a9fb8603eb72047d58dcb9994a6d495
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-03-28 12:34:04 +0300

    [#131049127] Assert that the svg is returned

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -21,5 +21,6 @@ class BadgeTestCase(BaseTestCase):
         sig = sig[:8].decode("utf-8")
         url = "/badge/%s/%s/foo.svg" % (self.alice.username, sig)
 
-        r = self.client.get(url)
+        r = self.client.get(url)        
         ### Assert that the svg is returned
+        self.assertEqual(r.status_code, 200) # should give a 200 if svg is found
