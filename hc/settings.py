b5d4e54cd1fec78398ba9021d1965b5f059e5b7c
malep2007 <ephraim.malinga@gmail.com>
2018-06-22 01:03:45 +0300

    travis modification

--- hc/settings.py
+++ hc/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
         'NAME':     'hc',
-        'USER':     'hc_ephraim',
+        'USER':     'postgres',
         'TEST': {'CHARSET': 'UTF8'}
     }
 }
