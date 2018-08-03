46e2a055b026991017e7167ca68c6ba44f06858a
masterabram <abramogol@gmail.com>
2018-08-03 08:57:41 +0300

    blog module

--- hc/blog/tests/test_views.py
+++
@@ -1,6 +0,0 @@
-from hc.test import BaseTestCase
-
-class ViewTestCase(BaseTestCase):
-    def test_it_uses_right_template(self):
-        r = self.all_blogs(self, '/blog/')
-        self.assertTemplateUsed(r, 'blog/index.html')
