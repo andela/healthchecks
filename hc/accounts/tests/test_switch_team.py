6db58273aad85bbf33a8f1c91e56c2e54b282949
Frank Odongkara <fodongkara@gmail.com>
2018-08-29 07:54:52 +0300

    working fixed clone

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -22,6 +22,7 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
         ### Assert the expected error code
+        assert r.status_code == 200
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
@@ -29,3 +30,4 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
         ### Assert the expected error code
+        assert r.status_code == 200
